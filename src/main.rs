6b4b42b344be62a5f66cacf514a92c1270167cbd
skie <skie@linuux.ru>
2025-08-30 20:33:31 +0000

    what? rust made files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,58 @@
 use std::char::from_u32;
 use rand;
 use rand::{Rng, thread_rng};
+//use clap::Parser
+//#[derive(Parser)]
 
+struct Todo{
+    description: String,
+    completed: bool,
+}
+struct TodoList{
+    todos: Vec<Todo>,
+}
+impl TodoList{
+    fn new() -> TodoList{
+        TodoList{todos: Vec::new()}
+        // making a vector incase something changes
+
+    }
+    fn add(&mut self, description: String){
+        let todo = Todo::new(description);
+        self.todos.push(todo);
+        // we make a new Todo
+        // then we push todo to todos 
+    }
+    fn remove(&mut self, index: usize){
+        if index < self.todos.len(){
+            self.todos.remove(index);
+        }
+    }
+    fn complete(&mut self, index: usize){
+        if index < self.todos.len(){
+            self.todos[index].mark_completed();
+        }
+    }
+    fn list(&self){
+        for(i, todo) in self.todos.iter().enumerate(){
+            let status = if todo.completed {"✔️"} else {"✖️"};
+            println!("{}: {} [{}]", i + 1, todo.description, status);
+        }
+    }
+}
+impl Todo {
+    fn new(description: String) -> Todo{
+        Todo{
+            // function that makes new todo 
+            description,
+            completed: false,
+        }
+    }
+    fn mark_completed(&mut self){
+        // if it completed
+        self.completed = true;
+    }
+}
 fn main() {
     println!("Hello, world!");
     let password_length: i32 = 15;
@@ -12,4 +63,5 @@ fn main() {
         result.push(ch);
     }
     println!("{}", result);
+
 }
